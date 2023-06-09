8fc486d995108694be3cf8b6770b24eb457de229
ttndev2 <ttndev2@gmail.com>
2023-06-09 08:05:58 +0700

    update url in background.js

--- public/background.js
+++ public/background.js
@@ -1,7 +1,7 @@
 try {
   chrome.action.onClicked.addListener((tab) => {
     if (tab.url === "chrome://newtab/") {
-      chrome.tabs.update(tab.id, { url: "https://your-url-here.com" }); // replace with your url
+      chrome.tabs.update(tab.id, { url: "https://dish-finder.vercel.app/" });
     } else {
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
